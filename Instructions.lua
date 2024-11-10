e401729b8d9a052ae9f1d852366e9b01bca3f3df
PancakeQuest <153967041+PancakequestYTZ@users.noreply.github.com>
2024-11-10 15:05:19 -0800

    Create Instructions.lua

---
+++ Instructions.lua
@@ -0,0 +1,114 @@
+--[[
+		\-|-░█████╗░██╗░░██╗██╗██╗░░░░░██╗░░░██╗███╗░░░███╗-|-/
+		\-|-██╔══██╗╚██╗██╔╝██║██║░░░░░██║░░░██║████╗░████║-|-/
+		\-|-███████║░╚███╔╝░██║██║░░░░░██║░░░██║██╔████╔██║-|-/
+		\-|-██╔══██║░██╔██╗░██║██║░░░░░██║░░░██║██║╚██╔╝██║-|-/
+		\-|-██║░░██║██╔╝╚██╗██║███████╗╚██████╔╝██║░╚═╝░██║-|-/
+		\-|-╚═╝░░╚═╝╚═╝░░╚═╝╚═╝╚══════╝░╚═════╝░╚═╝░░░░░╚═╝-|-/
+	
+	
+
+		Welcome to the Axilum Instructions Adminpanel
+		This is a open source project, developed by Januay051987 (pancakequest) and NoobNoobyNobNub (Nooby).
+		
+		
+		-------------------------------------------------------
+		|-█ █▄░█ █▀ ▀█▀ █▀█ █░█ █▀▀ ▀█▀ █ █▀█ █▄░█ █▀-|
+		|-█ █░▀█ ▄█ ░█░ █▀▄ █▄█ █▄▄ ░█░ █ █▄█ █░▀█ ▄█-|
+		-------------------------------------------------------
+		
+		To make a custom command, copy the Template Button Object.
+		Then edit the script in it to add actions to it. 
+		there's also existing commands in the Function List.
+		Remember this is open source
+		
+		-----------------------------------------------------------------------------------
+		|-█▀▀ ▀▄▀ █ █▀ ▀█▀ █ █▄░█ █▀▀   █▀▀ █▀█ █▀▄▀█ █▀▄▀█ ▄▀█ █▄░█ █▀▄ █▀ ▀-|
+		|-██▄ █░█ █ ▄█ ░█░ █ █░▀█ █▄█   █▄▄ █▄█ █░▀░█ █░▀░█ █▀█ █░▀█ █▄▀ ▄█ ▄-|
+		-----------------------------------------------------------------------------------
+		
+		ban 				Moderator
+		unban 				Moderator
+		kick  				Moderator
+		ping 				Member
+		:3 					Admin
+		OH_MY_PC			Admin
+		explode	(User)		Moderator
+		saturation (Integer)Admin
+		blur (Integer)		Admin
+		speed	(Integer)	Moderator
+		jump 				Moderator
+		health 				Admin
+		kill				Moderator
+		freeze				Moderator
+		unfreeze			Moderator
+		hide 				Moderator
+		show				Moderator
+		noclip 				Moderator
+		PotatoTroll			Admin
+		launch (User) 		Admin
+		God	(User) 			Admin
+		Ball 				Admin
+		Morph (User;ID)		Admin
+		ignite 	(User)		Moderator/VIP
+		Burn (Damage)		Moderator
+		Give (all/String)	Admin
+		TShirt (Image ID)	VIP
+		Shutdown 			Admin
+		Rank (User;Rank)	Admin
+		Joinlogs 			Admin
+		Clear 				Admin
+		Respawn (User)		Admin
+		Serverlock			Admin
+		Serverban 			Admin
+		Notify 				Admin
+		Warn (User;String)	Admin
+		Check (User)		Admin
+		View (User)			Admin
+		Track (User)		Admin
+		Untrack (User)		Admin
+		Viewlogs 			Admin
+		Mute 	(User)		Admin
+		Unmute 	(User)		Admin
+		Time 	(String)	Admin
+		To 		(User)		Admin
+		Bring (User) 		Admin
+		Teleport (Vector3)	Admin
+		BanLand 			Moderator
+		Countdown (String)	Admin
+		Message (String)	Admin
+		Blackhole (Vector3) Admin
+		Spam (User) 		Admin
+		Npc (Vector3)		Admin
+		Music (ID)			Admin
+		Unlife (User)		Admin
+		
+		
+		
+		
+		\-|-██╗░░░░░██╗███╗░░██╗██╗░░██╗░██████╗-|-/
+		\-|-██║░░░░░██║████╗░██║██║░██╔╝██╔════╝-|-/
+		\-|-██║░░░░░██║██╔██╗██║█████═╝░╚█████╗░-|-/
+		\-|-██║░░░░░██║██║╚████║██╔═██╗░░╚═══██╗-|-/
+		\-|-███████╗██║██║░╚███║██║░╚██╗██████╔╝-|-/
+		\-|-╚══════╝╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚═════╝░-|-/
+		
+		[Nooby's Group | https://www.roblox.com/groups/32669354/Nooby]
+		[Pancake's Group | https://www.roblox.com/groups/32669354/Nooby]
+		[Alixum Github Page] | https://github.com/PancakequestYTZ/Axilum] {WIP}
+		
+	
+		
+		\-|-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░-|-/
+		\-|-░█████╗░░█████╗░███╗░░░███╗███╗░░░███╗███████╗███╗░░██╗████████╗░█████╗░████████╗██╗░█████╗░███╗░░██╗-|-/
+		\-|-██╔══██╗██╔══██╗████╗░████║████╗░████║██╔════╝████╗░██║╚══██╔══╝██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║-|-/
+		\-|-██║░░╚═╝██║░░██║██╔████╔██║██╔████╔██║█████╗░░██╔██╗██║░░░██║░░░███████║░░░██║░░░██║██║░░██║██╔██╗██║-|-/
+		\-|-██║░░██╗██║░░██║██║╚██╔╝██║██║╚██╔╝██║██╔══╝░░██║╚████║░░░██║░░░██╔══██║░░░██║░░░██║██║░░██║██║╚████║-|-/
+   	    \-|-╚█████╔╝╚█████╔╝██║░╚═╝░██║██║░╚═╝░██║███████╗██║░╚███║░░░██║░░░██║░░██║░░░██║░░░██║╚█████╔╝██║░╚███║-|-/
+		\-|-░░ 	╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚-|-/
+		\-|-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░-|-/
+
+		[Hello]
+
+
+--]]
